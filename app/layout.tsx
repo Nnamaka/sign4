45afb595bb61c349ac60e4f3247bfabfec6993f8
unknown <nnamaka7@gmail.com>
2025-05-02 04:54:52 -0600

    added svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/excel-icon.svg', type: 'image/svg+xml' },
-      { url: '/favicon.ico', sizes: 'any' }
     ],
     apple: [
       { url: '/excel-icon.svg', type: 'image/svg+xml' }
